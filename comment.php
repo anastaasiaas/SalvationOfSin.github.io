0992bfeee441bb9dc920d31773a4a1cc16e2bfad
anastaasiaas <66118304+anastaasiaas@users.noreply.github.com>
2020-05-30 15:53:30 +0300

    Add files via upload

--- comment.php
+++ comment.php
@@ -1,10 +1,13 @@
 <?php
-  $name = $_POST["name"];
+  session_start();
+  $name = $_SESSION['email'];
   $text_comment = $_POST["text_comment"];
   $name = htmlspecialchars($name);
   $text_comment = htmlspecialchars($text_comment);
   $mysqli = new mysqli("localhost", "root", "", "qwer");
   $data = date('l jS \of F Y h:i:s A');
-  $mysqli->query("INSERT INTO `comments` (`name`, `comment`, `data`) VALUES ('$name', '$text_comment', '$data')");// Добавляем комментарий в таблицу
+  $i = 1;
+  $mysqli->query("INSERT INTO `comm` (`name`, `comment`, `data`) VALUES ('$name', '$text_comment', '$data')");
+  $i = $i+1;
   header("Location: ".$_SERVER["HTTP_REFERER"]);// Делаем реридект обратно
 ?>
--- forum.php
+++ forum.php
@@ -1,25 +1,35 @@
+<?php
+    require_once("header.php");
+?>
+
 <form name="comment" action="comment.php" method="post">
-  <p>
-    <label>Имя:</label>
-    <input type="text" name="name" />
-  </p>
   <p>
     <label>Комментарий:</label>
     <br />
-    <textarea name="text_comment" cols="30" rows="50"></textarea>
+    <textarea name="text_comment" cols="150" rows="10"></textarea>
   </p>
   <p>
     <input type="hidden" name="page_id" value="150" />
-    <input type="submit" value="Отправить" />
+    <input type="submit" value="Отправить"/>
   </p>
 </form>
+
+
+
 
 <?php
   $page_id = 150;// Уникальный идентификатор страницы (статьи или поста)
   $mysqli = new mysqli("localhost", "root", "", "qwer");// Подключается к базе данных
-  $result_set = $mysqli->query("SELECT * FROM `comments`"); //Вытаскиваем все комментарии для данной страницы
-  while ($row = $result_set->fetch_assoc()) {
-    print_r($row); //Вывод комментариев
-    echo "<br />";
+  $result_set = $mysqli->query("SELECT * FROM `comm` ORDER by id DESC"); //Вытаскиваем все комментарии для данной страницы
+  while ($row = $result_set->fetch_array()) {
+    echo "<div class='post-wrap'>
+	<div class='post-item'>
+		<div class='post-item-wrap'>
+			<a href='' class='post-link'>
+				<h3 class='post-title'>$row[1], $row[3]</h3>
+				<p class='post-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel arcu purus. Duis egestas purus et ex interdum lobortis. Quisque nec magna viverra, interdum dolor at, congue augue. Quisque sed interdum augue, a lacinia nulla. Sed risus nibh, vestibulum eu nisi eu, imperdiet tempor quam.</p>
+			</a>			
+		</div>
+	</div>";
   }
 ?>
